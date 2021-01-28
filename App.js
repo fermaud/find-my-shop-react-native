9f5442e076a32f772e8c6d881cb36e024ce4c1fb
fermaud <robin.fermaud@1d-lab.eu>
2021-01-28 23:41:35 +0100

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,45 +1,21 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
-import { enableScreens } from 'react-native-screens';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-
-import MealsNavigator from './navigation/MealsNavigator';
-import mealsReducer from './store/reducers/meals';
-
-enableScreens();
-
-const rootReducer = combineReducers({
-    meals: mealsReducer,
-});
-
-const store = createStore(rootReducer);
-
-const fetchFonts = () => {
-    return Font.loadAsync({
-        roboto: require('./assets/fonts/Roboto-Black.ttf'),
-        'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
-        'roboto-thin': require('./assets/fonts/Roboto-Thin.ttf'),
-    });
-};
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [fontLoaded, setFontLoaded] = useState(false);
-
-    if (!fontLoaded) {
-        return (
-            <AppLoading
-                startAsync={fetchFonts}
-                onFinish={() => setFontLoaded(true)}
-                onError={(err) => console.log(err)}
-            />
-        );
-    }
-    return (
-        <Provider store={store}>
-            <MealsNavigator />
-        </Provider>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
